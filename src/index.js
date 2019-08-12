9f531bbc4521c0eea40d31591c572d8204031509
Ruy <ruyjfs@gmail.com>
2019-08-12 02:18:15 -0300

    :ok_hand: Adicionando socket.io

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import express from 'express';
+import socket from 'socket.io'
+import cors from 'cors';
+
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
 
 import routes from './routes';
 
 let app = express();
 
+app.use(cors());
 app.use(routes);
 
 const port = process.env.PORT || 4000;
+// app.listen(port, console.log(`Running a GraphQL API server at localhost:${port}/graphql`));
+var io = socket.listen(app.listen(port));
 
-app.listen(port, console.log(`Running a GraphQL API server at localhost:${port}/graphql`));
+const connectedUsers = {};
+
+io.sockets.on('connection', function (socket) {
+    const { user } = socket.handshake.query;
+    connectedUsers[user] = socket.id;
+    console.log(connectedUsers);
+
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
+
